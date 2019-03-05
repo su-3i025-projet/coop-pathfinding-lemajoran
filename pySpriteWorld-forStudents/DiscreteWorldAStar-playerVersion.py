fe1dd443bf73d13a2069dc4562c00d4b14a6977a
zokaii <noe.zhang06@gmail.com>
2019-03-05 15:16:27 +0100

    A* les brooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooos

--- pySpriteWorld-forStudents/DiscreteWorldAStar-playerVersion.py
+++ pySpriteWorld-forStudents/DiscreteWorldAStar-playerVersion.py
@@ -12,11 +12,11 @@ from itertools import chain
 import pygame
 import glo
 
-import random 
+import random
 import numpy as np
 import sys
 
-
+from AStarAlgorithm import a_star
 # ---- ---- ---- ---- ---- ----
 # ---- Misc                ----
 # ---- ---- ---- ---- ---- ----
@@ -39,7 +39,7 @@ def init(_boardname=None):
     game.fps = 5  # frames per second
     game.mainiteration()
     player = game.player
-    
+
 def main():
 
     #for arg in sys.argv:
@@ -50,81 +50,67 @@ def main():
     print (iterations)
 
     init()
-    
 
-    
+
+
     #-------------------------------
     # Building the matrix
     #-------------------------------
-       
-           
+
+
     # on localise tous les états initiaux (loc du joueur)
     initStates = [o.get_rowcol() for o in game.layers['joueur']]
     print ("Init states:", initStates)
-    
+
     # on localise tous les objets ramassables
     goalStates = [o.get_rowcol() for o in game.layers['ramassable']]
     print ("Goal states:", goalStates)
-        
+
     # on localise tous les murs
     wallStates = [w.get_rowcol() for w in game.layers['obstacle']]
     #print ("Wall states:", wallStates)
-        
-    
-    #-------------------------------
-    # Building the best path with A*
-    #-------------------------------
-    
 
-    
-    
-        
-    #-------------------------------
-    # Moving along the path
-    #-------------------------------
-        
-    # bon ici on fait juste un random walker pour exemple...
-    
-
-    row,col = initStates[0]
+    start = initStates[0]
+    goal =goalStates[0]
     #row2,col2 = (5,5)
 
-    for i in range(iterations):
-    
-    
-        x_inc,y_inc = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
-        next_row = row+x_inc
-        next_col = col+y_inc
-        if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=20 and next_col>=0 and next_col<=20:
-            player.set_rowcol(next_row,next_col)
-            print ("pos 1:",next_row,next_col)
-            game.mainiteration()
+    res = a_star(start, goal, wallStates)
 
-            col=next_col
-            row=next_row
+    for i in res:
 
-            
-        
-            
-        # si on a  trouvé l'objet on le ramasse
-        if (row,col)==goalStates[0]:
-            o = game.player.ramasse(game.layers)
-            game.mainiteration()
-            print ("Objet trouvé!", o)
-            break
+        next_row, next_col = i
+        player.set_rowcol(next_row,next_col)
+        game.mainiteration()
+        # x_inc,y_inc = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
+        # next_row = row+x_inc
+        # next_col = col+y_inc
+        # if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=20 and next_col>=0 and next_col<=20:
+        #     player.set_rowcol(next_row,next_col)
+        #     print ("pos 1:",next_row,next_col)
+        #     game.mainiteration()
+        #
+        #     col=next_col
+        #     row=next_row
+        #
+        #
+        #
+        #
+        # # si on a  trouvé l'objet on le ramasse
+        # if (row,col)==goalStates[0]:
+        #     o = game.player.ramasse(game.layers)
+        #     game.mainiteration()
+        #     print ("Objet trouvé!", o)
+        #     break
         '''
         #x,y = game.player.get_pos()
-    
+
         '''
 
     pygame.quit()
-    
-        
-    
-   
+
+
+
+
 
 if __name__ == '__main__':
     main()
-    
-
-
